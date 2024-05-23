bcd12adf36081f95ed4800da22ac02571e2b7462
Arnob <raheearnob@gmail.com>
2024-05-23 18:59:03 -0400

    The csv file is now in directory

--- Facebook scrapping/Facebook scrapping.py
+++ Facebook scrapping/Facebook scrapping.py
@@ -1,40 +1,39 @@
 import os
 from facebook_page_scraper import Facebook_scraper
 
-page_list = ['antifaunited161']
+page_list = ['jackie.duvall']
 
 proxy_port = 10001
 
-posts_count = 100
+# Set posts_count to a large number to scrape as many posts as possible
+posts_count = 1000
 browser = "firefox"
 
 timeout = 600  # 600 seconds
 headless = False
 
-# Directory for output if we scrape directly to CSV
-directory = "E:\\facebook_scrape_results"
+# Directory for output set to the same directory as the script
+directory = os.getcwd()
 
-# Create the directory if it does not exist
+# Create the directory if it does not exist (this is redundant since we're using the current working directory)
 if not os.path.exists(directory):
     os.makedirs(directory)
 
 for page in page_list:
     # Our proxy for this scrape
     proxy = f'username:password@us.smartproxy.com:{proxy_port}'
+    
     # Initializing a scraper
     scraper = Facebook_scraper(page, posts_count, browser, proxy=proxy, timeout=timeout, headless=headless)
 
-    # Running the scraper in two ways:
+    # Configure the scraper to retrieve all comments, not just the first one
+    # Note: This assumes the facebook_page_scraper library supports these options.
+    try:
+        scraper.set_options(comments=True, all_comments=True)
+    except AttributeError:
+        print("Warning: The scraper does not support retrieving all comments. Check the library documentation.")
 
-    # 1
-    # Scraping and printing out the result into the console window:
-
-    # json_data = scraper.scrap_to_json()
-    # print(json_data)
-
-    # 2
-    # Scraping and writing into output CSV file:
-
+    # Scraping and writing into output CSV file
     filename = page
     scraper.scrap_to_csv(filename, directory)
 
